098fa3d29cf996d0c5f849b7a86aa1340879aa35
CashyO <seba1osick@gmail.com>
2026-04-07 00:03:58 -0400

    v6-navbar-styling

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,14 @@
 # URL config for posts app  
+# Flow: urls.py -> views.py -> templates
 
 from django.contrib import admin
 from django.urls import path
+from posts import views as views
 
 urlpatterns = [
-
+    # Home page url
+    path('', views.home, name='home'),
+    # Recipe posts page url
+    path('recipeposts', views.recipeposts, name='recipeposts')
+    
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Flow: urls.py -> views.py -> templates
+
+# Home page view
+def home(request):
+    title = "this is my title"
+    context = {'title': title}
+    return render(request, 'posts/home.html', context)
+
+# Recipe posts page view
+def recipeposts(request):
+    context = {}
+    return render(request, 'posts/recipeposts.html', context)
+
