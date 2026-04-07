2dd3cada6414b8f6f846b15ff4bc703034682a99
CashyO <seba1osick@gmail.com>
2026-04-06 21:45:11 -0400

    v3-db-basics

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+# Import the models.py tables
+from .models import * 
 
 # Register your models here.
+
+admin.site.register(RecipePost)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# Create a class for the Post database model 
+
+# Change these later 
+class RecipePost(models.Model): # Create table 
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title # used to identify the record
+    
